2d47b18626e2e0c34d760338b690487e033f3849
cipriantarlev <ciprian.tarlev@gmail.com>
2021-06-10 16:03:01 +0300

    adding redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,43 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-const App = () => {
+import { setSearchField, requestRobots } from '../actions';
 
-  const [robots, setRobots] = useState([]);
-  const [searchfield, setSearchfield] = useState('');
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(respone => respone.json())
-      .then(users => setRobots(users));
-  },[]); // run only once as componentDidMount
-
-  useEffect(() => {
-    console.log('count',count);
-  }, [count]) // only run if count changes.
-
-  const onSearchChange = (event) => {
-    setSearchfield(event.target.value);
+const mapStateToProps = (state) => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: () => dispatch(requestRobots())
+  }
+}
+
+const App = (props) => {
+  
+  const { 
+    searchField, 
+    onSearchChange, 
+    onRequestRobots, 
+    robots, 
+    isPending 
+  } = props;
+
+  useEffect(() => {
+    onRequestRobots();
+  },[onRequestRobots]);
 
   const filteredRobots = robots.filter(robot => {
-    return robot.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase());
+    return robot.name.toLocaleLowerCase().includes(searchField.toLocaleLowerCase());
   });
+  
 
-  return !robots.length ? <h1>Loading...</h1>
+  return isPending ? <h1>Loading...</h1>
     :
     (
       <div className='tc'>
         <h1 className='f1'>RoboFriends</h1>
-        <button 
-          onClick={() => setCount(count + 1)}
-        >Click Me!
-        </button>
         <SearchBox onSearchChange={onSearchChange} />
         <Scroll>
           <ErrorBoundry>
@@ -48,4 +58,4 @@ const App = () => {
     );
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
